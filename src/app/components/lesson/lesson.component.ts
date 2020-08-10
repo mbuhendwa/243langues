32465aebd1bb95312e00dafbb509bcc1e434a120
Michel Buhendwa <mkbuhendwa@gmail.com>
2020-08-10 14:06:34 -0400

    added table of content tree component

--- src/app/components/lesson/lesson.component.ts
+++ src/app/components/lesson/lesson.component.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { LessonService } from 'src/app/services/lesson.service';
 import { ActivatedRoute } from '@angular/router';
-import { MatTreeNestedDataSource } from '@angular/material/tree';
-import { NestedTreeControl } from '@angular/cdk/tree';
-
-interface LessonNode {
-  title: string;
-  url?: string;
-  chapters?: LessonNode[];
-}
 
 @Component({
   selector: 'app-lesson',
@@ -18,19 +10,13 @@ interface LessonNode {
 })
 export class LessonComponent implements OnInit {
   lesson: any;
-  tableTreeControl = new NestedTreeControl<LessonNode>(node => node.chapters);
-  contentTable = new MatTreeNestedDataSource<LessonNode>();
 
-  hasChild = (_: number, node: LessonNode) => !!node.chapters && node.chapters.length > 0;
-  
   getLesson = (id: string) => this.lessonService.getLesson(id).subscribe(response => (this.lesson = response.data()));
-  getTable = () => this.lessonService.getContentTable().subscribe(response => (this.contentTable.data = response.data().i));
 
   constructor(private lessonService: LessonService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.getLesson(this.route.snapshot.params.id);
-    this.getTable();
   }
 
 }
