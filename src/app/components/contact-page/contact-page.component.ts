40400c333df1c728522cbf8ed74200fbca53af4d
Michel Buhendwa <mkbuhendwa@gmail.com>
2020-09-13 02:53:50 -0400

    simple contact page

--- src/app/components/contact-page/contact-page.component.ts
+++ src/app/components/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,16 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
-  options: Array<any> = [
-    'Suggestion',
-    'Other reasons'
-  ];
-  contact_success: Boolean = true;
-  message: any = { success: "Successfully sent email.", error: "Failed to send message!" };
+  // options: Array<any> = [
+  //   'Suggestion',
+  //   'Other reasons'
+  // ];
+  // contact_success: Boolean = true;
+  // message: any = { success: "Successfully sent email.", error: "Failed to send message!" };
 
-  constructor() { }
+  constructor(private title: Title, private meta: Meta) { }
 
   ngOnInit(): void {
+    this.title.setTitle("Contact - 243Langues");
+    this.meta.updateTag({name: "description", content: "Contactez 243Langues pour toutes suggestions, commentaires, avis,... par email. Envoyez un email à contact@243langues.org."})
   }
 
 }
